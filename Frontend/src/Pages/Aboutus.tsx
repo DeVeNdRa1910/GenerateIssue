487cd6d21182b27143ef8e99761d591b62602cfc
Devendra <devendrav1910@gmail.com>
2025-03-09 13:35:05 +0530

    update somthing

--- Frontend/src/Pages/Aboutus.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Aboutus() {
-  return (
-    <div>This About us page</div>
-  )
-}
-
-export default Aboutus
--- Frontend/src/config/conf.ts
+++
@@ -1,7 +0,0 @@
-
-const conf = {
-    clerk_api_key: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string,
-    clerk_secret_key: import.meta.env.VITE_CLERK_SECRET_KEY as string,
-}
-
-export default conf;
